ac05465c6728ef9043c71e2f534d4a4408f85a4b
margotj38 <john-elie.margot@ensimag.grenoble-inp.fr>
2015-05-26 18:07:46 +0200

    ajout de tout

---
+++ EtudeEvenement/EtudeEvenement/ChoixSeuilFenetre.vb
@@ -0,0 +1,60 @@
+﻿Public Class ChoixSeuilFenetre
+
+    Private textSeuil As String
+    Private textFenetreDebut As String
+    Private textFenetreFin As String
+
+    Public modele As Integer = -1 '-1 => probleme; 0 => ModeleMoyenne; 1 => ModeleRentaMarche; 2 => ModeleMarche
+
+    Private Sub ChoixBox_TextChanged(sender As Object, e As EventArgs) Handles ChoixBox.TextChanged
+        textSeuil = ChoixBox.Text
+    End Sub
+
+    Private Sub FenetreBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreBox.TextChanged
+        textFenetreDebut = FenetreBox.Text
+    End Sub
+
+    Private Sub FenetreFinBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreFinBox.TextChanged
+        textFenetreFin = FenetreFinBox.Text
+    End Sub
+
+    Private Sub Ok_Click(sender As Object, e As EventArgs) Handles Ok.Click
+        Try
+            Dim seuil As Double = CDbl(textSeuil)
+            If seuil >= 1 Or seuil <= 0 Then
+                MsgBox("Erreur : Le seuil doit être compris entre 0 et 1", 16)
+            Else
+                Dim fenetreDebut As Integer = CInt(textFenetreDebut)
+                Dim fenetreFin As Integer = CInt(textFenetreFin)
+                Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
+                Dim premiereDate As Integer = currentSheet.Cells(2, 1).Value
+                If fenetreDebut <= premiereDate Or fenetreFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 Then
+                    MsgBox("Erreur : La fenêtre de temps de l'événement doit être cohérente avec les données", 16)
+                Else
+                    Dim rejet As Boolean
+                    Select Case modele
+                        Case 0
+                            rejet = Globals.ThisAddIn.ThisAddIn_CalcNormMoy(fenetreDebut, fenetreFin, seuil)
+                        Case 1
+                            rejet = Globals.ThisAddIn.ThisAddIn_ModeleRentaMarche(fenetreDebut, seuil)
+                        Case 2
+                            rejet = Globals.ThisAddIn.ThisAddIn_ModeleMarche(fenetreDebut, seuil)
+                        Case Else
+                            MsgBox("Erreur interne : Provient de ChoixSeuilFenetre.vb", 16)
+                    End Select
+                    Dim pValeur As Double = Globals.ThisAddIn.ThisAddIn_PValeur(modele, fenetreDebut)
+                    MsgBox("P-Valeur : " & pValeur)
+                    Globals.Ribbons.Ruban.seuilFenetreTaskPane.Visible = False
+                    If rejet Then
+                        MsgBox("Rejet de l'hypothèse")
+                    Else
+                        MsgBox("Non rejet de l'hypothèse")
+                    End If
+                End If
+            End If
+        Catch erreur As InvalidCastException
+            MsgBox("Erreur : Vous devez entrer des données correctes (utiliser la virgule pour les nombres décimaux)", 16)
+        End Try
+    End Sub
+
+End Class
---
+++ EtudeEvenement/EtudeEvenement/ThisAddIn.vb
@@ -0,0 +1,205 @@
+﻿Public Class ThisAddIn
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+    'Calcul la statistique de test et effectue le test
+    'Renvoie true si l'hypothèse est rejetée
+    Public Function ThisAddIn_MethodeTabCAR(tabAR As Double(,), seuil As Double, fenetreDebut As Integer, fenetreFin As Integer) As Boolean
+        Dim varCAR(tabAR.GetLength(1)) As Double                     'Variable aléatoire correspondant aux CAR
+        Dim tailleFenetre As Integer = fenetreFin - fenetreDebut + 1
+
+        'Calcul de la statistique pour chaque entreprise
+        For colonne = 0 To tabAR.GetUpperBound(1)
+            'Calcul de CAR sur la fenetre d'événement
+            Dim CAR As Double = 0
+            Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
+            Dim indDebFenetre As Integer = 2 + fenetreDebut - currentSheet.Cells(2, 1).Value
+            Dim indFinFenetre As Integer = 2 + fenetreFin - currentSheet.Cells(2, 1).Value
+            For i = indDebFenetre - 2 To indFinFenetre - 2
+                CAR = CAR + tabAR(i, colonne)
+            Next i
+            Dim moyenne As Double = 0
+            For i = 0 To indDebFenetre - 1 - 2
+                moyenne = moyenne + tabAR(i, colonne)
+            Next i
+            moyenne = moyenne / (indDebFenetre - 2)
+            'Calcul de la variance des AR sur la période d'estimation
+            Dim variance As Double = 0
+            For i = 0 To indDebFenetre - 1 - 2
+                Dim tmp As Double = tabAR(i, colonne) - moyenne
+                variance = variance + tmp * tmp
+            Next i
+            variance = variance / (indDebFenetre - 2)
+            varCAR(colonne) = CAR / Math.Sqrt(tailleFenetre * variance)
+        Next colonne
+
+        'Test statistique
+        ThisAddIn_MethodeTabCAR = test_student(varCAR, tabAR.GetLength(1), seuil)
+    End Function
+
+    Public Function ThisAddIn_ModeleMarche(fenetre As Integer, seuil As Double) As Boolean
+        'on se positionne sur la feuille des Rt
+        Dim currentSheet As Excel.Worksheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+        'compte le nombre de lignes et de colonnes
+        Dim nbLignes As Integer = currentSheet.UsedRange.Rows.Count
+        Dim nbColonnes As Integer = currentSheet.UsedRange.Columns.Count
+        'tableau stockant les AR calculés grâce à la régression
+        Dim tabAR(nbLignes - 2, nbColonnes - 2) As Double
+        'indice de ligne de la dernière ligne de l'ensemble d'apprentissage
+        Dim dernLigne As Integer = nbLignes - fenetre
+
+        For i = 0 To nbColonnes - 2
+            Dim plageY As Excel.Range
+            Dim plageX As Excel.Range
+            plageY = Application.Range(currentSheet.Cells(2, i + 2), currentSheet.Cells(dernLigne, i + 2))
+            'on se positionne sur la feuille des Rm pour récupérer plageX
+            currentSheet = CType(Application.Worksheets("Rm"), Excel.Worksheet)
+            plageX = Application.Range(currentSheet.Cells(2, i + 2), currentSheet.Cells(dernLigne, i + 2))
+            'calcul des paramètres de la régression linéaire
+            Dim beta As Double = Application.WorksheetFunction.Index(Application.WorksheetFunction.LinEst(plageY, plageX), 1)
+            Dim alpha As Double = Application.WorksheetFunction.Index(Application.WorksheetFunction.LinEst(plageY, plageX), 2)
+
+            'remplissage du tableau
+            For t = 0 To nbLignes - 2
+                Dim k As Double = alpha + beta * currentSheet.Cells(t + 2, i + 2).Value
+                currentSheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+                tabAR(t, i) = currentSheet.Cells(t + 2, i + 2).Value - k
+                currentSheet = CType(Application.Worksheets("Rm"), Excel.Worksheet)
+            Next
+            'on retourne sur la feuille des Rt
+            currentSheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+        Next
+        'ThisAddIn_ModeleMarche = ThisAddIn_MethodeTabCAR(tabAR, seuil, fenetre)
+        Return False
+    End Function
+
+    'Récupère les Rt et les Rm, puis appelle les calculs de statistique
+    Public Function ThisAddIn_ModeleRentaMarche(fenetre As Integer, seuil As Double) As Boolean
+        Dim currentSheet As Excel.Worksheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+        'compte le nombre de lignes et de colonnes
+        Dim nbLignes As Integer = currentSheet.UsedRange.Rows.Count
+        Dim nbColonnes As Integer = currentSheet.UsedRange.Columns.Count
+        'tableau stockant les AR calculés grâce à la régression
+        Dim tabAR(nbLignes - 2, nbColonnes - 2) As Double
+
+        For i = 0 To nbColonnes - 2
+            'remplissage du tableau
+            For t = 0 To nbLignes - 2
+                currentSheet = CType(Application.Worksheets("Rm"), Excel.Worksheet)
+                Dim k As Double = currentSheet.Cells(t + 2, i + 2).Value
+                currentSheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+                tabAR(t, i) = currentSheet.Cells(t + 2, i + 2).Value - k
+            Next
+        Next
+
+        'ThisAddIn_ModeleRentaMarche = ThisAddIn_MethodeTabCAR(tabAR, seuil, fenetre) 'on se positionne sur la feuille des Rt
+        Return False
+    End Function
+
+    'Calcul les Ki, puis effectue les tests statistiques sur (Ri - Ki)
+    'Renvoie true si l'hypothèse est rejetée
+    Public Function ThisAddIn_CalcNormMoy(fenetreDebut As Integer, fenetreFin As Integer, seuil As Double) As Boolean
+        Dim currentSheet As Excel.Worksheet = CType(Application.Worksheets("Rt"), Excel.Worksheet)
+        Dim nbLignes As Integer = currentSheet.UsedRange.Rows.Count                'Nombre de lignes
+        Dim nbColonnes As Integer = currentSheet.UsedRange.Columns.Count           'Nombre de colonnes
+        Dim tabMoy(nbColonnes - 2) As Double                                       'Tableau des moyennes de chaque titre
+        Dim tailleFenetre As Integer = fenetreFin - fenetreDebut + 1
+
+        'Calcul des moyennes
+        For colonne = 2 To nbColonnes
+            Dim plage As Excel.Range = Application.Range(currentSheet.Cells(2, colonne), currentSheet.Cells(nbLignes - tailleFenetre, colonne))
+            tabMoy(colonne - 2) = Application.WorksheetFunction.Average(plage)
+        Next colonne
+
+        'Calcul des AR sur la fenêtre
+        Dim tabAR(nbLignes - 2, nbColonnes - 2) As Double                          'Tableau des AR sur la fenêtre de l'événement
+        'Dim debFenetre As Integer = nbLignes - fenetre + 1
+        For colonne = 2 To nbColonnes
+            For indDate = 2 To nbLignes
+                tabAR(indDate - 2, colonne - 2) = currentSheet.Cells(indDate, colonne).Value - tabMoy(colonne - 2)
+            Next indDate
+        Next colonne
+        ThisAddIn_CalcNormMoy = ThisAddIn_MethodeTabCAR(tabAR, seuil, fenetreDebut, fenetreFin)
+    End Function
+
+    'Renvoie true si l'hypothèse est rejetée
+    Public Function ThisAddIn_MethodeCAR(seuil As Double) As Boolean
+        Dim activeSheet As Excel.Worksheet = CType(Application.ActiveSheet, Excel.Worksheet)
+        Dim nbLignes As Integer = activeSheet.UsedRange.Rows.Count                'Nombre de lignes
+        Dim nbColonnes As Integer = activeSheet.UsedRange.Columns.Count           'Nombre de colonnes
+        Dim varCAR(nbColonnes - 2) As Double                                      'Variable aléatoire correspondant aux CAR
+
+        'Calcul de la statistique pour chaque entreprise
+        For colonne = 2 To nbColonnes
+            Dim plage As Excel.Range = Application.Range(Application.Cells(2, colonne), Application.Cells(nbLignes, colonne))
+            Dim CAR As Double = Application.WorksheetFunction.Sum(plage)
+            Dim variance As Double = Application.WorksheetFunction.Var(plage)
+            varCAR(colonne - 2) = CAR / Math.Sqrt(variance * (nbLignes - 1))
+        Next colonne
+
+        'Test statistique
+        ThisAddIn_MethodeCAR = test_student(varCAR, nbColonnes - 1, seuil)
+    End Function
+
+    Public Function ThisAddIn_PValeur(modele As Integer, Optional fenetreDebut As Integer = 0, Optional fenetreFin As Integer = 0) As Double
+        Dim borneInf As Double = 0
+        Dim borneSup As Double = 1
+        Dim alpha As Double = (borneInf + borneSup) / 2
+        While borneSup - borneInf > 0.000001
+            Dim rejet As Boolean
+            Select Case modele
+                Case 3
+                    rejet = ThisAddIn_MethodeCAR(alpha)
+                Case 0
+                    rejet = ThisAddIn_CalcNormMoy(fenetreDebut, fenetreFin, alpha)
+                Case 1
+                    rejet = ThisAddIn_ModeleRentaMarche(fenetreDebut, alpha)
+                Case 2
+                    rejet = ThisAddIn_ModeleMarche(fenetreDebut, alpha)
+                Case Else
+                    MsgBox("Erreur interne : Provient de ThisAddIn_ThinAddIn_PValeur", 16)
+            End Select
+            If rejet Then
+                borneSup = alpha
+            Else
+                borneInf = alpha
+            End If
+            alpha = (borneInf + borneSup) / 2
+        End While
+        Return alpha
+    End Function
+
+    'Renvoie true si l'hypothèse H0 est rejetée
+    Private Function test_student(tabCAR() As Double, tailleTabCAR As Integer, seuil As Double) As Boolean
+        test_student = False
+        Dim testHypothese As Double
+        Dim moyenneTab As Double = calcul_moyenne(tabCAR)
+        Dim varianceTab As Double = calcul_variance(tabCAR, moyenneTab)
+        testHypothese = Math.Abs(Math.Sqrt(tailleTabCAR) * moyenneTab / varianceTab)
+        If testHypothese > Application.WorksheetFunction.TInv(seuil, tailleTabCAR - 1) Then
+            test_student = True
+        End If
+    End Function
+
+    Private Function calcul_moyenne(tab() As Double) As Double
+        For i = 0 To tab.GetUpperBound(0)
+            calcul_moyenne = calcul_moyenne + tab(i)
+        Next i
+        calcul_moyenne = calcul_moyenne / (tab.GetLength(0))
+    End Function
+
+    Private Function calcul_variance(tab() As Double, moyenne As Double) As Double
+        For i = 0 To tab.GetUpperBound(0)
+            Dim tmp As Double = tab(i) - moyenne
+            calcul_variance = calcul_variance + tmp * tmp
+        Next i
+        calcul_variance = calcul_variance / (tab.GetLength(0) - 1)
+    End Function
+
+End Class
