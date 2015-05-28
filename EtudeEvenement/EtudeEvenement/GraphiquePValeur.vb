02f0db42b15ec170acef64d048759cf67726edcb
muleta33 <antoine.mulet@ensimag.grenoble-inp.fr>
2015-05-28 14:39:46 +0200

    enregistrer graphique

--- EtudeEvenement/EtudeEvenement/GraphiquePValeur.vb
+++ EtudeEvenement/EtudeEvenement/GraphiquePValeur.vb
@@ -1,3 +1,14 @@
-﻿Public Class GraphiquePValeur
+﻿Imports System.Drawing.Imaging.ImageFormat
+
+Public Class GraphiquePValeur
+
+    Private Sub SaveGraph_Click_1(sender As Object, e As EventArgs) Handles SaveGraph.Click
+        SaveFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As ComponentModel.CancelEventArgs) Handles SaveFileDialog1.FileOk
+        Dim fileToSave As String = SaveFileDialog1.FileName
+        Me.GraphiqueChart.SaveImage(fileToSave, Png)
+    End Sub
 
 End Class
