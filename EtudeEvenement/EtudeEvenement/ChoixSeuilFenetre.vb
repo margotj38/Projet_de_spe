3ab87cabfcb7e03fbf98498aab0ec191ae3c3db6
margotj38 <john-elie.margot@ensimag.grenoble-inp.fr>
2015-05-31 19:08:32 +0200

    gestion de la période d'estimation parametrable

--- EtudeEvenement/EtudeEvenement/ChoixSeuilFenetre.vb
+++ EtudeEvenement/EtudeEvenement/ChoixSeuilFenetre.vb
@@ -3,8 +3,11 @@ Imports System.Windows.Forms.DataVisualization.Charting
 
 Public Class ChoixSeuilFenetre
 
-    Private textFenetreDebut As String
-    Private textFenetreFin As String
+    Private textFenetreEstDebut As String
+    Private textFenetreEstFin As String
+    Private textFenetreEvDebut As String
+    Private textFenetreEvFin As String
+    Private ajustementEst As Boolean = False
 
     Private model As Integer ' 0 => ModeleMoyenne; 1 => ModeleMarcheSimple; 2 => ModeleMarche
     Private numTest As Integer ' 0 => TestSimple; 1 => TestPatell; 2 => TestSigne
@@ -42,48 +45,66 @@ Public Class ChoixSeuilFenetre
         End Set
     End Property
 
+    Private Sub FenetreEstDebBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreEstDebBox.TextChanged
+        textFenetreEstDebut = FenetreEstDebBox.Text
+    End Sub
+
+    Private Sub FenetreEstFinBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreEstFinBox.TextChanged
+        textFenetreEstFin = FenetreEstFinBox.Text
+    End Sub
+
     Private Sub FenetreDebBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreDebBox.TextChanged
-        textFenetreDebut = FenetreDebBox.Text
+        textFenetreEvDebut = FenetreDebBox.Text
     End Sub
 
     Private Sub FenetreFinBox_TextChanged(sender As Object, e As EventArgs) Handles FenetreFinBox.TextChanged
-        textFenetreFin = FenetreFinBox.Text
+        textFenetreEvFin = FenetreFinBox.Text
+    End Sub
+
+    Private Sub Ajustement_CheckedChanged(sender As Object, e As EventArgs) Handles Ajustement.CheckedChanged
+        ajustementEst = Not ajustementEst
     End Sub
 
     Private Sub PValeur_Click(sender As Object, e As EventArgs) Handles PValeur.Click
         Try
-            Dim fenetreDebut As Integer = CInt(textFenetreDebut)
-            Dim fenetreFin As Integer = CInt(textFenetreFin)
+            Dim fenetreEstDebut As Integer = CInt(textFenetreEstDebut)
+            Dim fenetreEstFin As Integer = CInt(textFenetreEstFin)
+            Dim fenetreEvDebut As Integer = CInt(textFenetreEvDebut)
+            Dim fenetreEvFin As Integer = CInt(textFenetreEvFin)
+
             Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
             Dim premiereDate As Integer = currentSheet.Cells(2, 1).Value
             Dim tailleEchant As Integer = currentSheet.UsedRange.Columns.Count - 1
-            If fenetreDebut > fenetreFin Or fenetreDebut <= premiereDate Or fenetreFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 Then
+
+            If fenetreEvDebut > fenetreEvFin Or fenetreEvFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 _
+                Or fenetreEstDebut > fenetreEstFin Or fenetreEstDebut < premiereDate Or fenetreEstFin >= fenetreEvDebut Then
+                MsgBox(fenetreEstDebut & " " & fenetreEstFin & " " & fenetreEvDebut & " " & fenetreEvFin)
                 MsgBox("Erreur : La fenêtre de temps de l'événement doit être cohérente avec les données", 16)
             Else
                 'Calcul des AR
                 Dim tabAR As Double(,)
-                tabAR = Globals.ThisAddIn.calculAR(fenetreDebut)
-                For i = 0 To tabAR.GetUpperBound(0)
-                    Debug.WriteLine(tabAR(i, 0))
-                Next
+                tabAR = Globals.ThisAddIn.calculAR(fenetreEstDebut, fenetreEstFin)
+                'For i = 0 To tabAR.GetUpperBound(0)
+                'Debug.WriteLine(tabAR(i, 0))
+                'Next
                 'Calcul de la pValeur
                 Dim pValeur As Double
                 Select Case test
                     Case 0
                         'test simple'
                         Dim tabCAR As Double()
-                        tabCAR = Globals.ThisAddIn.calculCAR(tabAR, fenetreDebut, fenetreFin)
+                        tabCAR = Globals.ThisAddIn.calculCAR(tabAR, fenetreEstDebut, fenetreEstFin, fenetreEvDebut, fenetreEvFin)
                         Dim testHyp As Double = Globals.ThisAddIn.calculStatistique(tabCAR)
                         pValeur = Globals.ThisAddIn.calculPValeur(tailleEchant, testHyp) * 100
                     Case 1
                         'test de Patell'
-                        Dim testHyp As Double = Globals.ThisAddIn.patellTest(tabAR, fenetreDebut, fenetreFin)
+                        Dim testHyp As Double = Globals.ThisAddIn.patellTest(tabAR, fenetreEstDebut, fenetreEstFin, fenetreEvDebut, fenetreEvFin)
                         pValeur = 2 * (1 - Globals.ThisAddIn.Application.WorksheetFunction.Norm_S_Dist(Math.Abs(testHyp), True)) * 100
                     Case 2
                         'test de signe : à compléter'
                         pValeur = 0
                 End Select
-                
+
                 MsgBox("P-Valeur : " & pValeur.ToString("0.0000") & "%")
                 Globals.Ribbons.Ruban.seuilFenetreTaskPane.Visible = False
             End If
@@ -94,19 +115,22 @@ Public Class ChoixSeuilFenetre
 
     Private Sub PValeurFenetre_Click(sender As Object, e As EventArgs) Handles PValeurFenetre.Click
         Try
-            Dim fenetreDebut As Integer = CInt(textFenetreDebut)
-            Dim fenetreFin As Integer = CInt(textFenetreFin)
-            Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
-            Dim tailleEchant As Integer = currentSheet.UsedRange.Columns.Count - 1
-            Dim premiereDate As Integer = currentSheet.Cells(2, 1).Value
-            Dim derniereDate As Integer = premiereDate + currentSheet.UsedRange.Rows.Count - 2
-            Dim maxFenetre As Integer = Math.Min(Math.Abs(premiereDate), Math.Abs(derniereDate))
+            Dim fenetreEstDebut As Integer = CInt(textFenetreEstDebut)
+            Dim fenetreEstFin As Integer = CInt(textFenetreEstFin)
+            Dim fenetreEvDebut As Integer = CInt(textFenetreEvDebut)
+            Dim fenetreEvFin As Integer = CInt(textFenetreEvFin)
 
-            If fenetreDebut > fenetreFin Or fenetreDebut <= premiereDate Or fenetreFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 Then
+            Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
+            Dim premiereDate As Integer = currentSheet.Cells(2, 1).Value
+            Dim tailleEchant As Integer = currentSheet.UsedRange.Columns.Count - 1
+
+            If fenetreEvDebut > fenetreEvFin Or fenetreEvFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 _
+                Or fenetreEstDebut > fenetreEstFin Or fenetreEstDebut < premiereDate Or fenetreEstFin >= fenetreEvDebut Then
+                MsgBox(fenetreEstDebut & " " & fenetreEstFin & " " & fenetreEvDebut & " " & fenetreEvFin)
                 MsgBox("Erreur : La fenêtre de temps de l'événement doit être cohérente avec les données", 16)
             Else
                 'Calcul des pvaleurs et affichage de la courbe
-                Globals.ThisAddIn.tracerPValeur(tailleEchant, maxFenetre)
+                Globals.ThisAddIn.tracerPValeur(tailleEchant, ajustementEst, fenetreEstDebut, fenetreEstFin, fenetreEvDebut, fenetreEvFin)
                 Globals.Ribbons.Ruban.seuilFenetreTaskPane.Visible = False
                 Globals.Ribbons.Ruban.graphPVal.Visible = True
             End If
@@ -114,30 +138,5 @@ Public Class ChoixSeuilFenetre
             MsgBox("Erreur : Vous devez entrer des données correctes (utiliser la virgule pour les nombres décimaux)", 16)
         End Try
     End Sub
-
-    'Private Sub BoutonPatell_Click(sender As Object, e As EventArgs) Handles BoutonPatell.Click
-    '    Try
-    '        Dim fenetreDebut As Integer = CInt(textFenetreDebut)
-    '        Dim fenetreFin As Integer = CInt(textFenetreFin)
-    '        Dim currentSheet As Excel.Worksheet = CType(Globals.ThisAddIn.Application.Worksheets("Rt"), Excel.Worksheet)
-    '        Dim premiereDate As Integer = currentSheet.Cells(2, 1).Value
-    '        Dim tailleEchant As Integer = currentSheet.UsedRange.Columns.Count - 1
-    '        If fenetreDebut > fenetreFin Or fenetreDebut <= premiereDate Or fenetreFin > premiereDate + currentSheet.UsedRange.Rows.Count - 1 Then
-    '            MsgBox("Erreur : La fenêtre de temps de l'événement doit être cohérente avec les données", 16)
-    '        Else
-    '            'Calcul de la pvaleur
-    '            Dim tabAR As Double(,)
-    '            tabAR = Globals.ThisAddIn.calculAR(fenetreDebut)
-
-    '            Dim testHyp As Double = Globals.ThisAddIn.patellTest(tabAR, fenetreDebut, fenetreFin)
-    '            Dim pValeur As Double = 2 * (1 - Globals.ThisAddIn.Application.WorksheetFunction.Norm_S_Dist(Math.Abs(testHyp), True)) * 100
-
-    '            MsgBox("P-Valeur : " & pValeur.ToString("0.0000") & "%")
-    '            Globals.Ribbons.Ruban.seuilFenetreTaskPane.Visible = False
-    '        End If
-    '    Catch erreur As InvalidCastException
-    '        MsgBox("Erreur : Vous devez entrer des données correctes (utiliser la virgule pour les nombres décimaux)", 16)
-    '    End Try
-    'End Sub
 
 End Class
