c02ec778e4f209454ddbb47ca1248c8db4fd7ad9
muleta33 <antoine.mulet@ensimag.grenoble-inp.fr>
2015-06-08 12:38:55 +0200

    correction bug

--- EtudeEvenement/EtudeEvenement/TestsStatistiques.vb
+++ EtudeEvenement/EtudeEvenement/TestsStatistiques.vb
@@ -94,6 +94,7 @@
                 Dim tmp As Double = tab(i) - moyenne
                 'On somme en multipliant par le nombre de #N/A présents + 1 (ie prixPresent)
                 calcul_variance = calcul_variance + tmp * tmp * prixPresent
+                prixPresent = 1
             End If
         Next i
         'On divise par la taille de la fenêtre d'estimation - 1, moins le nombre de #N/A finaux (ie prixPresent - 1)
